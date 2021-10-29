89f8d4cce38527958d2e3330da2b89bac0bbe241
Oscar Rodriguez <oscarrodriguez101@gmail.com>
2021-10-28 21:13:25 -0400

    contract improvements and optimizations

--- src/contracts/Correlations.sol
+++ src/contracts/Correlations.sol
@@ -1,18 +1,17 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Correlations is ERC721, Ownable {
+    using Counters for Counters.Counter;
 
-    mapping(address=>uint[]) collections;
-    struct Token {
-        uint8 id;
-    }
-    Token[] mintedTokens;
+    Counters.Counter private _tokenCount;
     string baseUri;
     uint maxSupply;
+    bool sale;
 
     constructor() ERC721("CORE", "Correlations") Ownable() {
         maxSupply = 50;
@@ -22,36 +21,54 @@ contract Correlations is ERC721, Ownable {
         baseUri = _newUri;
     }
 
-    function mintToken() public payable returns(uint){
-        require(msg.value == (0.1 ether), 'Please send 0.1 ether');
-        require((mintedTokens.length) < maxSupply, 'Token Limit Reached');
+    function saleSwitch() public onlyOwner {
+        sale = !sale;
+    }
 
-        uint8 tokenId = uint8(mintedTokens.length + 1);
+    modifier forSale() {
+        require(sale, "Currently not minting");
+        _;
+    }
+
+    function mintToken() public payable forSale returns(uint){
+        require(msg.value == (0.1 ether), 'Please send 0.1 ether');
+        require(_tokenCount.current() < maxSupply, 'Token Limit Reached');
+
+        _tokenCount.increment();
+
+        uint tokenId = _tokenCount.current();
         _safeMint(msg.sender, tokenId);
-        collections[msg.sender].push(tokenId);
-        mintedTokens.push(Token(tokenId));
 
         return tokenId;
     }
 
-    function mintTokenMulti() public payable returns(uint8[] memory){
+    function mintTokenMulti() public payable forSale returns(uint[] memory){
         require(msg.value == (0.5 ether), 'Please send 0.5 ether for Multi Mint');
-        require((mintedTokens.length + 4) < maxSupply, 'Token Limit Reached or Not Enough Available');
+        require((_tokenCount.current() + 4) < maxSupply, 'Token Limit Reached or Not Enough Available');
+        _tokenCount.increment();
 
-        uint8 startingTokenId = uint8(mintedTokens.length + 1);
-        uint8[] memory tokenIds = new uint8[](5);
+        uint startingTokenId = _tokenCount.current();
+        uint[] memory tokenIds = new uint[](5);
         for(uint i = 0; i < tokenIds.length; i++){
             uint tokenId = startingTokenId + i;
             _safeMint(msg.sender, tokenId);
-            mintedTokens.push(Token(uint8(tokenId)));
-            collections[msg.sender].push(tokenId);
-            tokenIds[i] = uint8(tokenId);
+            tokenIds[i] = tokenId;
+            _tokenCount.increment();
         }
         return tokenIds;
     }
 
-    function getCollection() public view returns(uint[] memory){
-        return collections[msg.sender];
+    function getCollection(address _address) public view returns(uint[] memory){
+        uint count = balanceOf(_address); //6
+        uint[] memory ownedTokens = new uint[](count); // [0:0, 1:0, 2:0, 3:0, 4:0, 5:0]
+        uint found = 0;
+        for (uint i = 1; i < _tokenCount.current(); i++){
+            if(ownerOf(i) == _address){
+                ownedTokens[found] = i;
+                found++;
+            }
+        }
+        return ownedTokens;
     }
 
     function withdraw() public onlyOwner {
@@ -59,7 +76,7 @@ contract Correlations is ERC721, Ownable {
         payable(msg.sender).transfer(balance);
     }
     
-
+    //for donations etc...
     receive() external payable {}
 
 }
